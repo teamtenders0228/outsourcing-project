6de0d9e9e792ba409b7f22c14065a23469d9c4a5
lamgak12 <tntnqk12@naver.com>
2025-03-04 14:06:35 +0900

    ErrorCode에 가게 이름 중복 추가

--- src/main/java/com/example/outsourcingproject/common/exception/ErrorCode.java
+++ src/main/java/com/example/outsourcingproject/common/exception/ErrorCode.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //유저 관련 에러 코드
     //가게 관련 에러 코드
+    INVALID_CATEGORY_TYPE("유효하지 않은 카테고리 이름입니다.", HttpStatus.BAD_REQUEST),
+    CONFLICT_STORE_NAME("동일한 가게이름이 존재합니다.",HttpStatus.CONFLICT),
     //주문 관련 에러 코드
     //리뷰 관련 에러 코드
     //그 외 에러 코드

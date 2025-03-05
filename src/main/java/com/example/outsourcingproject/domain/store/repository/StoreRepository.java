102413afa18bb12dac254c961f97405fb76cf821
lamgak12 <tntnqk12@naver.com>
2025-03-05 12:51:56 +0900

    refactor: 논리적 삭제에 의한 쿼리 변경

--- src/main/java/com/example/outsourcingproject/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/outsourcingproject/domain/store/repository/StoreRepository.java
@@ -4,6 +4,7 @@ import com.example.outsourcingproject.domain.store.entity.Store;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +12,15 @@ import java.util.Optional;
 public interface StoreRepository extends JpaRepository<Store, Long> {
     boolean existsByStoreName(String storeName);
 
-    int countByUserId(Long userId);
-    @Query("SELECT s FROM Store s WHERE s.closedFlag = true")
+    @Query("SELECT COUNT(s) FROM Store s WHERE s.user.id = :userId AND s.deleteAt IS NULL")
+    int countByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT s FROM Store s WHERE s.closedFlag = true AND s.deleteAt IS NULL")
     @EntityGraph(attributePaths = "user")
     List<Store> findAllOpenStores();
 
 
+    @Query("SELECT s FROM Store s WHERE s.id = :storeId AND s.deleteAt IS NULL")
     @EntityGraph(attributePaths = "user")
     Optional<Store> getStoreById(Long storeId);
 }

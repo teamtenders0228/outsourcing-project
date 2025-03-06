d7d63a998ac0b03ab548bfaca11685acfb10f075
hyeon22 <ski0123@naver.com>
2025-03-06 19:25:24 +0900

    test : 테스트 코드 추가 AuthService, MenuService, UserService 테스트 코드 추가

--- src/test/java/com/example/outsourcingproject/domain/user/service/UserServiceTest.java
+++ src/test/java/com/example/outsourcingproject/domain/user/service/UserServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.outsourcingproject.domain.user.service;
 
+import com.example.outsourcingproject.common.dto.AuthUser;
 import com.example.outsourcingproject.common.encoder.PasswordEncoder;
+import com.example.outsourcingproject.common.exception.BaseException;
 import com.example.outsourcingproject.domain.user.dto.request.ChangePasswordRequestDto;
+import com.example.outsourcingproject.domain.user.dto.request.UserDeleteRequestDto;
 import com.example.outsourcingproject.domain.user.dto.response.UserResponseDto;
 import com.example.outsourcingproject.domain.user.entity.User;
-import com.example.outsourcingproject.domain.user.entity.UserRole;
+import com.example.outsourcingproject.domain.user.enums.UserRole;
 import com.example.outsourcingproject.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -37,13 +40,14 @@ class UserServiceTest {
     private UserService userService;
 
     @Test
-    void getUser_존재하는_사용자_정보_조회를_성공한다() {
+    void findUser_존재하는_사용자_정보_조회를_성공한다() {
         // given
         Long userId = 1L;
-        User mockUser = new User("John Doe", "abcd1234!Q",
-                "john@example.com", "010-1234-5678", "Seoul", UserRole.USER);
+        User user = new User("John Doe", "john@example.com","abcd1234!Q",
+                 "010-1234-5678", "Seoul", UserRole.USER);
+        ReflectionTestUtils.setField(user, "id", userId);
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
 
-        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
         //when
         UserResponseDto responseDto = userService.findUser(userId);
 
@@ -57,29 +61,46 @@ class UserServiceTest {
     @Test
     void changePassword_비밀번호_변경_성공한다() {
         // given
-        String oldPassword = "oldPassword";
-        String newPassword = "newPassword";
-        String preEncodedPassword = "preEncodedPassword";
-        String encodedPassword = "encodedPassword";
-        ChangePasswordRequestDto request = new ChangePasswordRequestDto(oldPassword, newPassword);
-        User mockUser = new User("John Doe", "abcd1234!Q",
-                "john@example.com", "010-1234-5678", "Seoul", UserRole.USER);
-        ReflectionTestUtils.setField(mockUser, "id", 1L);
+        String oldPassword = "oldPassword11!";
+        String newPassword = "newPassword11!";
 
-        given(userRepository.findById(anyLong())).willReturn(Optional.of(mockUser));
-        given(mockUser.getDeleteFlag()).willReturn(false);
-        given(passwordEncoder.matches(newPassword, mockUser.getPassword())).willReturn(false);
-        given(passwordEncoder.matches(oldPassword, mockUser.getPassword())).willReturn(true);
-        given(passwordEncoder.encode(newPassword)).willReturn(encodedPassword);
+        ChangePasswordRequestDto request = new ChangePasswordRequestDto(oldPassword, newPassword);
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.OWNER);
+
+        User user = new User("John Doe", "john@example.com", oldPassword,
+                "010-1234-5678", "Seoul", UserRole.USER);
+        ReflectionTestUtils.setField(user, "id", authUser.getId());
+
+        given(userRepository.findById(1L)).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(request.getOldPassword(), user.getPassword())).willReturn(true);
+        given(passwordEncoder.encode(newPassword)).willReturn("encodedNewPassword");
 
         // when
-        userService.changePassword(mockUser.getId(), request);
+        userService.changePassword(authUser.getId(), request);
 
         // then
-        then(userRepository).should(times(1)).findById(1L);
-        then(passwordEncoder).should().matches(newPassword, preEncodedPassword);
-        then(passwordEncoder).should().matches(oldPassword, preEncodedPassword);
-        then(passwordEncoder).should().encode(newPassword);
-        assertEquals(encodedPassword, mockUser.getPassword());
+        assertEquals(user.getPassword(), "encodedNewPassword");
+    }
+
+    @Test
+    void deleteUserd에서_입력한_비밀번호와_기존비밀번호가_다를_경우_예외를_던진다() {
+        // given
+        String password = "Password11!";
+        AuthUser authUser = new AuthUser(1L, "email", UserRole.OWNER);
+
+        User user = new User("John Doe", "john@example.com", password,
+                "010-1234-5678", "Seoul", UserRole.USER);
+        ReflectionTestUtils.setField(user, "id", authUser.getId());
+
+        UserDeleteRequestDto userDeleteRequestDto = new UserDeleteRequestDto(password);
+
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches(userDeleteRequestDto.getPassword(), user.getPassword())).willReturn(false);
+
+        // when & then
+        assertThrows(BaseException.class,
+                () -> userService.deleteUser(authUser.getId(), userDeleteRequestDto.getPassword()),
+                "잘못된 비밀번호입니다."
+        );
     }
 }
